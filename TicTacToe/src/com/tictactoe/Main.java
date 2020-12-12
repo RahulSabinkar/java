239b8509833a869dd9979e0d568cdb1bf4664391
Rahul Sabinkar <rahulsabinkar4u@gmail.com>
2020-12-12 17:54:22 +0000

    Completed 2-Player TicTacToe

--- TicTacToe/src/com/tictactoe/Main.java
+++ TicTacToe/src/com/tictactoe/Main.java
@@ -11,68 +11,133 @@ public class Main {
                 {'-', '-', '-', '-', '-' },
                 {' ', '|', ' ', '|', ' ' },
          };
+         placePiece(gameBoard);
+    }
+
+    static void placePiece(char[][] gameBoard) {
         Scanner input = new Scanner(System.in);
+        print(gameBoard);
         for (byte i = 9; i > 0; --i) {
-            if (i%2!=0)
-                System.out.print("Enter your turn X (1-9): ");
-            else
-                System.out.print("Enter your turn O (1-9): ");
+            char symbol;
+            if (i%2!=0) symbol = 'X';
+            else symbol = 'O';
+            System.out.print("Enter your turn "+symbol+" (1-9): ");
             byte playerTurn = input.nextByte();
-            if (i%2!=0) {
-                switch (playerTurn) {
-                    case 1 -> {
-                        gameBoard[0][0] = 'X';
-//                        check(gameBoard, 1);
-                    }
-                    case 2 -> gameBoard[0][2] = 'X';
-                    case 3 -> gameBoard[0][4] = 'X';
-                    case 4 -> gameBoard[2][0] = 'X';
-                    case 5 -> gameBoard[2][2] = 'X';
-                    case 6 -> gameBoard[2][4] = 'X';
-                    case 7 -> gameBoard[4][0] = 'X';
-                    case 8 -> gameBoard[4][2] = 'X';
-                    case 9 -> gameBoard[4][4] = 'X';
-                    default -> {
+            switch (playerTurn) {
+                case 7:
+                    if (gameBoard[0][0] == ' ')
+                        gameBoard[0][0] = symbol;
+                    else {
                         System.out.println("Invalid turn!");
                         i++;
                     }
-                }
-            }
-            else  {
-                switch (playerTurn) {
-                    case 1 -> gameBoard[0][0] = 'O';
-                    case 2 -> gameBoard[0][2] = 'O';
-                    case 3 -> gameBoard[0][4] = 'O';
-                    case 4 -> gameBoard[2][0] = 'O';
-                    case 5 -> gameBoard[2][2] = 'O';
-                    case 6 -> gameBoard[2][4] = 'O';
-                    case 7 -> gameBoard[4][0] = 'O';
-                    case 8 -> gameBoard[4][2] = 'O';
-                    case 9 -> gameBoard[4][4] = 'O';
-                    default -> {
+                    break;
+                case 8:
+                    if (gameBoard[0][2] == ' ')
+                        gameBoard[0][2] = symbol;
+                    else {
                         System.out.println("Invalid turn!");
                         i++;
                     }
-                }
+                    break;
+                case 9:
+                    if (gameBoard[0][4] == ' ')
+                        gameBoard[0][4] = symbol;
+                    else {
+                        System.out.println("Invalid turn!");
+                        i++;
+                    }
+                    break;
+                case 4:
+                    if (gameBoard[2][0] == ' ')
+                        gameBoard[2][0] = symbol;
+                    else {
+                        System.out.println("Invalid turn!");
+                        i++;
+                    }
+                    break;
+                case 5:
+                    if (gameBoard[2][2] == ' ')
+                        gameBoard[2][2] = symbol;
+                    else {
+                        System.out.println("Invalid turn!");
+                        i++;
+                    }
+                    break;
+                case 6:
+                    if (gameBoard[2][4] == ' ')
+                        gameBoard[2][4] = symbol;
+                    else {
+                        System.out.println("Invalid turn!");
+                        i++;
+                    }
+                    break;
+                case 1:
+                    if (gameBoard[4][0] == ' ')
+                        gameBoard[4][0] = symbol;
+                    else {
+                        System.out.println("Invalid turn!");
+                        i++;
+                    }
+                    break;
+                case 2:
+                    if (gameBoard[4][2] == ' ')
+                        gameBoard[4][2] = symbol;
+                    else {
+                        System.out.println("Invalid turn!");
+                        i++;
+                    }
+                    break;
+                case 3:
+                    if (gameBoard[4][4] == ' ')
+                        gameBoard[4][4] = symbol;
+                    else {
+                        System.out.println("Invalid turn!");
+                        i++;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid turn!");
+                    i++;
+                    break;
             }
             print(gameBoard);
+            if (i<=5) check(gameBoard, symbol);
         }
+        System.out.println("It's a DRAW!");
     }
-    // static void check(char[][] gameBoard, byte x) {
-
-    //     for (int i = 0; i < 5; i+=2) {
-    //         for (int j=0; j < 5; j+=2) {
-    //         }
-    //         System.out.print("\n");
-    //     }
-    // }
 
     static void print(char[][] gameBoard) {
         for (int i = 0; i < 5; i++) {
-            for (int j=0; j < 5; j++) {
+            for (int j=0; j < 5; j++)
                 System.out.print(gameBoard[i][j]);
-            }
             System.out.print("\n");
+        }
+    }
+
+    static void check(char[][] gameBoard, char symbol) {
+        //HORIZONTAL LOOP
+        for (int i=0; i<5; i+=2) {
+            if (symbol == gameBoard[i][0] && symbol== gameBoard[i][2] && symbol == gameBoard[i][4]) {
+                System.out.println("Player "+symbol+" has WON!");
+                System.exit(0);
+            }
+        }
+        // VERTICAL LOOP
+        for (int i=0; i<5; i+=2) {
+            if (symbol == gameBoard[0][i] && symbol == gameBoard[2][i] && symbol== gameBoard[4][i]) {
+                System.out.println("Player "+symbol+" has WON!");
+                System.exit(0);
+            }
+        }
+        // DIAGONAL CROSSES
+        if (symbol == gameBoard[0][0] && symbol == gameBoard[2][2] && symbol== gameBoard[4][4]) {
+            System.out.println("Player "+symbol+" has WON!");
+            System.exit(0);
+        }
+        if (symbol == gameBoard[4][0] && symbol == gameBoard[2][2] && symbol == gameBoard[0][4]) {
+            System.out.println("Player "+symbol+" has WON!");
+            System.exit(0);
         }
     }
 }
